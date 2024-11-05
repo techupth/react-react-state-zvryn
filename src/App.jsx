044ce5df84f8ce485b30c5545b9065c3b06d83c9
zvryn <sarinkanok@gmail.com>
2024-11-05 11:30:22 +0700

    feat:complete assignment react state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message");
+
+  const handleThaiClick = () => {
+    setGreetingMessage("สวัสดี");
+  };
+  const handlHiClick = () => {
+    setGreetingMessage("Hi!");
+  };
+  const handleChineseClick = () => {
+    setGreetingMessage("你好!");
+  };
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => handleThaiClick()}>สวัสดี!</button>
+        <button onClick={() => handlHiClick()}>Hi!</button>
+        <button onClick={() => handleChineseClick()}>你好!</button>
       </div>
     </div>
   );
